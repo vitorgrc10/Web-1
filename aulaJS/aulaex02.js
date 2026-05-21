c88c25c1ce9d2cb37d67c30b40b5ba087d12879f
vitorgrc10 <vitorgrc@unipam.edu.br>
2026-05-21 00:13:39 +0000

    aula 20/05

---
+++ aulaJS/aulaex02.js
@@ -0,0 +1,15 @@
+function saudacao(nome){
+    return "Olá, " + nome + "!";
+}
+
+let frase = saudacao("Vitor");
+
+console.log(frase);
+
+
+function soma(a, b){
+    return A * B;
+}
+
+let frase = soma(4 * 5);
+console.log("O resultado da some é: " + resultado);
